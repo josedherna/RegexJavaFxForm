36c835767d07c355d6d9ee00764045515f556872
josedherna <157747148+josedherna@users.noreply.github.com>
2025-04-14 23:38:07 -0400

    feat: applied css styling to button and text fields

--- src/main/java/org/csc311/regexjavafxform/StartApplication.java
+++ src/main/java/org/csc311/regexjavafxform/StartApplication.java
@@ -14,7 +14,7 @@ public class StartApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("form-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
