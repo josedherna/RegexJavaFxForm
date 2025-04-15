f82b016cfe76257038033aee72ba37f00470add3
josedherna <157747148+josedherna@users.noreply.github.com>
2025-04-14 23:22:14 -0400

    feat: created layout of registration screen

--- src/main/java/org/csc311/regexjavafxform/FormController.java
+++ src/main/java/org/csc311/regexjavafxform/FormController.java
@@ -1,9 +1,36 @@
 package org.csc311.regexjavafxform;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.regex.*;
 
 /**
  * @author Jose Hernandez
  */
 public class FormController {
+    @FXML
+    private Label firstNamePrompt;
+    @FXML
+    private Label lastNamePrompt;
+    @FXML
+    private Label dobPrompt;
+    @FXML
+    private Label emailPrompt;
+    @FXML
+    private TextField zipcodeField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private Label zipcodePrompt;
+    @FXML
+    private Button addButton;
+    @FXML
+    private TextField lastNameField;
+    @FXML
+    private TextField firstNameField;
+    @FXML
+    private TextField dobField;
 }
